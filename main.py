d25ea8f394534f9c14fd3debeb9d4af4029ae92a
leo-pfeiffer <leopold.pfeiffer@icloud.com>
2024-11-02 11:31:07 -0400

    Setup

--- main.py
+++ main.py
@@ -2,17 +2,27 @@ import os
 import json
 import logging
 import requests
-from datetime import date
+from datetime import datetime
 from base64 import b64encode
+import pprint
+
+logging.basicConfig(level=logging.INFO)
 
 with open('config.json') as f:
     config = json.load(f)
 
 repositories = config.get('repositories')
 owner = config.get('owner')
-token = os.getenv('GITHUB_TOKEN')
+token = os.getenv('PAT_GITHUB')
 
-current_date = date.today().strftime('%Y-%m-%d')
+if not token:
+    raise Exception('No GitHub Personal Access Token provided')
+
+timestamp = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+
+logging.info(f'Updating files in {owner}\'s repositories')
+logging.info(f'Current timestamp: {timestamp}')
+logging.info(f'Updating repositories: {pprint.pformat(repositories)}')
 
 for repository in repositories:
     name = repository.get('name')
@@ -24,16 +34,27 @@ for repository in repositories:
         'Content-Type': 'application/json'
     }
 
-    content = b64encode(current_date.encode()).decode()
+    # Check if the file exists
+    response = requests.get(url, headers=headers)
+    if response.status_code == 200:
+        sha = response.json().get('sha')
+        logging.info(f'{file} exists in {name}')
+    else:
+        sha = None
+
+    content = b64encode(timestamp.encode()).decode()
     data = {
-        'message': f'Update {file} to {current_date}',
+        'message': f'Update {file} to {timestamp}',
         'content': content,
         'branch': 'main'
     }
 
+    if sha:
+        data['sha'] = sha
+
     response = requests.put(url, headers=headers, json=data)
 
     if response.status_code in [200, 201]:
-        logging.info(f'Successfully created or updated {file} in {name}')
+        logging.info(f'Successfully updated {file} in {name}')
     else:
-        logging.error(f'Failed to create or update {file} in {name}: {response.json()}')
+        raise Exception(f'Failed update {file} in {name}: {response.json()}')
